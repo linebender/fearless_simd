03c88ac5382285f5475c4f2311f53e9807828680
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-07-31 13:46:23 +0200

    Fix some warnings

--- fearless_simd_dev_macros/src/lib.rs
+++ fearless_simd_dev_macros/src/lib.rs
@@ -3,34 +3,80 @@
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{ItemFn, parse_macro_input};
 
-/// Create test for checking consistency between different SIMD backends. 
+/// Create test for checking consistency between different SIMD backends.
 #[proc_macro_attribute]
 pub fn simd_test(_: TokenStream, item: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(item as ItemFn);
-
     let input_fn_name = input_fn.sig.ident.clone();
-    let scalar_name = Ident::new(
-        &format!("{input_fn_name}_scalar"),
-        input_fn_name.span(),
-    );
-    let neon_name = Ident::new(
-        &format!("{input_fn_name}_scalar"),
-        input_fn_name.span(),
-    );
-    
-    let scalar_snippet =  quote! {
-        #input_fn
-        
-        #[test]
-        fn #scalar_name() {
-            let fallback = fearless_simd::Fallback::new();
-            #input_fn_name(fallback);
-        }
+
+    let get_ident =
+        |name: &str| Ident::new(&format!("{input_fn_name}_{name}"), input_fn_name.span());
+
+    let fallback_name = get_ident("fallback");
+    let neon_name = get_ident("neon");
+
+    let include_fallback = !exclude_fallback(&input_fn_name.to_string());
+    let include_neon = {
+        #[cfg(not(target_arch = "aarch64"))]
+        let temp = false;
+        #[cfg(target_arch = "aarch64")]
+        let temp = std::arch::is_aarch64_feature_detected!("neon");
+
+        temp & !exclude_neon(&input_fn_name.to_string())
     };
-    
+
+    let fallback_snippet = if include_fallback {
+        quote! {
+            #[test]
+            fn #fallback_name() {
+                let fallback = fearless_simd::Fallback::new();
+                #input_fn_name(fallback);
+            }
+        }
+    } else {
+        quote! {}
+    };
+
+    let neon_snippet = if include_neon {
+        quote! {
+            #[test]
+            fn #neon_name() {
+                let neon = unsafe { fearless_simd::Neon::new_unchecked() };
+                #input_fn_name(neon);
+            }
+        }
+    } else {
+        quote! {}
+    };
+
     quote! {
-        #scalar_snippet
-    }.into()
+        #input_fn
+
+        #fallback_snippet
+        #neon_snippet
+    }
+    .into()
+}
+
+// You can update below functions if you want to exclude certain tests from different architectures
+// (for example because they haven't been implemented yet).
+
+#[allow(dead_code, reason = "on purpose.")]
+#[allow(unused_variables, reason = "on purpose.")]
+fn exclude_neon(name: &str) -> bool {
+    false
+}
+
+#[allow(dead_code, reason = "on purpose.")]
+#[allow(unused_variables, reason = "on purpose.")]
+fn exclude_fallback(name: &str) -> bool {
+    false
+}
+
+#[allow(dead_code, reason = "on purpose.")]
+#[allow(unused_variables, reason = "on purpose.")]
+fn exclude_wasm(name: &str) -> bool {
+    false
 }
--- fearless_simd_tests/tests/new.rs
+++ fearless_simd_tests/tests/new.rs
@@ -1,10 +1,10 @@
+use fearless_simd::{Simd, SimdBase, f32x4};
 use fearless_simd_dev_macros::simd_test;
-use fearless_simd::{f32x4, Simd, SimdBase};
 
 #[simd_test]
 fn add_f32<S: Simd>(simd: S) {
     let a = f32x4::from_slice(simd, &[1.0, 2.0, 3.0, 4.0]);
     let b = f32x4::from_slice(simd, &[4.0, 3.0, 2.0, 1.0]);
-    
+
     assert_eq!((a + b).val, [5.0, 5.0, 5.0, 5.0])
 }
