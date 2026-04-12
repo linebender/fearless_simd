b8e4adcafb12abbaea60a42a42b68b6fe340b60e
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-04-12 11:45:13 +0100

    cargo fmt

--- fearless_simd_tests/tests/harness/mod.rs
+++ fearless_simd_tests/tests/harness/mod.rs
@@ -1833,10 +1833,15 @@ fn interleave_u32x8<S: Simd>(simd: S) {
 
 #[simd_test]
 fn interleave_u8x16<S: Simd>(simd: S) {
-    let a = u8x16::from_slice(simd, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+    let a = u8x16::from_slice(
+        simd,
+        &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    );
     let b = u8x16::from_slice(
         simd,
-        &[16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+        &[
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+        ],
     );
     let (x, y) = simd.interleave_u8x16(a, b);
     assert_eq!(*x, [0, 16, 1, 17, 2, 18, 3, 19, 4, 20, 5, 21, 6, 22, 7, 23]);
