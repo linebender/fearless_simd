6f1081fdd226f87fd20a58b4098a6b5c1b6046db
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-05-24 22:07:31 +0100

    KISS the native type mask roundtrip tests

--- fearless_simd_tests/tests/harness/lm_generated/mask_roundtrip_x86.rs
+++ fearless_simd_tests/tests/harness/lm_generated/mask_roundtrip_x86.rs
@@ -5,36 +5,11 @@
 use core::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use core::arch::x86_64::*;
+use core::convert::TryFrom;
+use core::mem::size_of;
 
 use fearless_simd::*;
 use fearless_simd_dev_macros::simd_test;
-
-const INTERESTING_32: &[u64] = &[
-    0x0000_0000,
-    0x0000_0001,
-    0x8000_0000,
-    0x0000_ffff,
-    0xffff_0000,
-    0x5555_5555,
-    0xaaaa_aaaa,
-    0x8000_aa55,
-    0xffff_ffff,
-    0xffff_ffff_0000_0000,
-    0xffff_ffff_8000_aa55,
-    0xffff_ffff_ffff_ffff,
-];
-
-const INTERESTING_64: &[u64] = &[
-    0x0000_0000_0000_0000,
-    0x0000_0000_0000_0001,
-    0x8000_0000_0000_0000,
-    0x0000_0000_ffff_ffff,
-    0xffff_ffff_0000_0000,
-    0x5555_5555_5555_5555,
-    0xaaaa_aaaa_aaaa_aaaa,
-    0x8000_0001_5555_aaab,
-    0xffff_ffff_ffff_ffff,
-];
 
 fn lane_mask(lanes: usize) -> u64 {
     if lanes == u64::BITS as usize {
@@ -44,197 +19,239 @@ fn lane_mask(lanes: usize) -> u64 {
     }
 }
 
-trait MaskArch: Copy + Eq + core::fmt::Debug {
-    fn from_bits(bits: u64) -> Self;
+fn lanes_from_bits<L, const LANES: usize>(bits: u64) -> [L; LANES]
+where
+    L: Copy + From<i8>,
+{
+    let bits = bits & lane_mask(LANES);
+    core::array::from_fn(|i| {
+        if ((bits >> i) & 1) != 0 {
+            L::from(-1)
+        } else {
+            L::from(0)
+        }
+    })
 }
 
-impl MaskArch for u8 {
-    fn from_bits(bits: u64) -> Self {
-        Self::try_from(bits).expect("masked bits fit in __mmask8")
+fn assert_native_vector_roundtrip<S, M, A, L, const LANES: usize>(simd: S, bits: u64)
+where
+    S: Simd,
+    M: SimdMask<S> + SimdFrom<A, S> + Into<A>,
+    A: Copy,
+    L: Copy + Eq + core::fmt::Debug + From<i8>,
+{
+    let expected_bits = bits & lane_mask(LANES);
+    let expected_lanes = lanes_from_bits::<L, LANES>(bits);
+
+    assert_eq!(
+        size_of::<A>(),
+        size_of::<[L; LANES]>()
+    );
+
+    let mask = M::from_bitmask(simd, bits);
+    let arch: A = mask.into();
+    // Safety: the size assertion above verifies that the x86 vector type has
+    // the same size as the signed integer lane representation used for masks.
+    let lanes = unsafe { core::mem::transmute_copy::<A, [L; LANES]>(&arch) };
+    assert_eq!(lanes, expected_lanes);
+
+    // Safety: this builds the native x86 vector value from the lane
+    // representation expected by the public mask conversion.
+    let arch = unsafe { core::mem::transmute_copy::<[L; LANES], A>(&expected_lanes) };
+    let mask = M::simd_from(simd, arch);
+    assert_eq!(mask.to_bitmask(), expected_bits);
+}
+
+fn assert_native_mask_roundtrip<S, M, A, const LANES: usize>(simd: S, bits: u64)
+where
+    S: Simd,
+    M: SimdMask<S> + SimdFrom<A, S> + Into<A>,
+    A: Copy + Eq + core::fmt::Debug + TryFrom<u64>,
+    A::Error: core::fmt::Debug,
+{
+    let expected_bits = bits & lane_mask(LANES);
+    let expected_arch = A::try_from(expected_bits).expect("masked bits fit in native mask type");
+
+    let mask = M::from_bitmask(simd, bits);
+    let arch: A = mask.into();
+    assert_eq!(arch, expected_arch);
+
+    let mask = M::simd_from(simd, expected_arch);
+    assert_eq!(mask.to_bitmask(), expected_bits);
+
+    let arch: A = mask.into();
+    assert_eq!(arch, expected_arch);
+}
+
+#[simd_test]
+fn mask8x16_m128i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask8x16<S>, __m128i, i8, 16>(simd, bits);
     }
 }
 
-impl MaskArch for u16 {
-    fn from_bits(bits: u64) -> Self {
-        Self::try_from(bits).expect("masked bits fit in __mmask16")
+#[simd_test]
+fn mask16x8_m128i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask16x8<S>, __m128i, i16, 8>(simd, bits);
     }
 }
 
-impl MaskArch for u32 {
-    fn from_bits(bits: u64) -> Self {
-        Self::try_from(bits).expect("masked bits fit in __mmask32")
+#[simd_test]
+fn mask32x4_m128i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask32x4<S>, __m128i, i32, 4>(simd, bits);
     }
 }
 
-impl MaskArch for u64 {
-    fn from_bits(bits: u64) -> Self {
-        bits
+#[simd_test]
+fn mask64x2_m128i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask64x2<S>, __m128i, i64, 2>(simd, bits);
     }
 }
 
-macro_rules! assert_native_vector_roundtrip {
-    ($simd:expr, $mask:ident, $arch:ty, $lane:ty, $lanes:literal, $bits:expr) => {{
-        let bits = $bits;
-        let expected_bits = bits & lane_mask($lanes);
-        let expected_lanes: [$lane; $lanes] = core::array::from_fn(|i| {
-            if ((expected_bits >> i) & 1) != 0 {
-                -1
-            } else {
-                0
-            }
-        });
-
-        let mask = $mask::from_bitmask($simd, bits);
-        let arch: $arch = mask.into();
-        // Safety: these x86 vector types have the same size and lane layout as the signed
-        // integer arrays used for mask values.
-        let lanes = unsafe { core::mem::transmute::<$arch, [$lane; $lanes]>(arch) };
-        assert_eq!(
-            lanes,
-            expected_lanes,
-            "{} -> {} lane values for {bits:#018x}",
-            stringify!($mask),
-            stringify!($arch)
-        );
-
-        // Safety: this builds the native x86 vector value from the lane representation expected
-        // by the public mask conversion.
-        let arch = unsafe { core::mem::transmute::<[$lane; $lanes], $arch>(expected_lanes) };
-        let mask = $mask::simd_from($simd, arch);
-        assert_eq!(
-            mask.to_bitmask(),
-            expected_bits,
-            "{} <- {} bitmask for {bits:#018x}",
-            stringify!($mask),
-            stringify!($arch)
-        );
-    }};
+#[simd_test]
+fn mask8x32_m256i_roundtrip<S: Simd>(simd: S) {
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0x0000_0000);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0x0000_0001);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0x8000_0000);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0x0000_ffff);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0xffff_0000);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0x5555_5555);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0xaaaa_aaaa);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0x8000_aa55);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0xffff_ffff);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0xffff_ffff_0000_0000);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0xffff_ffff_8000_aa55);
+    assert_native_vector_roundtrip::<S, mask8x32<S>, __m256i, i8, 32>(simd, 0xffff_ffff_ffff_ffff);
 }
 
-macro_rules! assert_native_mask_roundtrip {
-    ($simd:expr, $mask:ident, $arch:ty, $lanes:literal, $bits:expr) => {{
-        let bits = $bits;
-        let expected_bits = bits & lane_mask($lanes);
-        let expected_arch = <$arch as MaskArch>::from_bits(expected_bits);
-
-        let mask = $mask::from_bitmask($simd, bits);
-        let arch: $arch = mask.into();
-        assert_eq!(
-            arch,
-            expected_arch,
-            "{} -> {} for {bits:#018x}",
-            stringify!($mask),
-            stringify!($arch)
-        );
-
-        let mask = $mask::simd_from($simd, expected_arch);
-        assert_eq!(
-            mask.to_bitmask(),
-            expected_bits,
-            "{} <- {} bitmask for {bits:#018x}",
-            stringify!($mask),
-            stringify!($arch)
-        );
-
-        let arch: $arch = mask.into();
-        assert_eq!(
-            arch,
-            expected_arch,
-            "{} -> {} after roundtrip for {bits:#018x}",
-            stringify!($mask),
-            stringify!($arch)
-        );
-    }};
+#[simd_test]
+fn mask16x16_m256i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask16x16<S>, __m256i, i16, 16>(simd, bits);
+    }
 }
 
-macro_rules! native_vector_roundtrip_exhaustive {
-    ($test:ident, $mask:ident, $arch:ty, $lane:ty, $lanes:literal) => {
-        #[simd_test]
-        fn $test<S: Simd>(simd: S) {
-            for bits in 0..=0xffff_u64 {
-                assert_native_vector_roundtrip!(simd, $mask, $arch, $lane, $lanes, bits);
-            }
-        }
-    };
+#[simd_test]
+fn mask32x8_m256i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask32x8<S>, __m256i, i32, 8>(simd, bits);
+    }
 }
 
-macro_rules! native_vector_roundtrip_interesting {
-    ($test:ident, $mask:ident, $arch:ty, $lane:ty, $lanes:literal, $values:ident) => {
-        #[simd_test]
-        fn $test<S: Simd>(simd: S) {
-            for &bits in $values {
-                assert_native_vector_roundtrip!(simd, $mask, $arch, $lane, $lanes, bits);
-            }
-        }
-    };
+#[simd_test]
+fn mask64x4_m256i_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_vector_roundtrip::<S, mask64x4<S>, __m256i, i64, 4>(simd, bits);
+    }
 }
 
-macro_rules! native_mask_roundtrip_exhaustive {
-    ($test:ident, $mask:ident, $arch:ty, $lanes:literal) => {
-        #[simd_test]
-        fn $test<S: Simd>(simd: S) {
-            for bits in 0..=0xffff_u64 {
-                assert_native_mask_roundtrip!(simd, $mask, $arch, $lanes, bits);
-            }
-        }
-    };
+#[simd_test]
+fn mask8x16_mmask16_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask8x16<S>, __mmask16, 16>(simd, bits);
+    }
 }
 
-macro_rules! native_mask_roundtrip_interesting {
-    ($test:ident, $mask:ident, $arch:ty, $lanes:literal, $values:ident) => {
-        #[simd_test]
-        fn $test<S: Simd>(simd: S) {
-            for &bits in $values {
-                assert_native_mask_roundtrip!(simd, $mask, $arch, $lanes, bits);
-            }
-        }
-    };
+#[simd_test]
+fn mask16x8_mmask8_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask16x8<S>, __mmask8, 8>(simd, bits);
+    }
 }
 
-native_vector_roundtrip_exhaustive!(mask8x16_m128i_roundtrip, mask8x16, __m128i, i8, 16);
-native_vector_roundtrip_exhaustive!(mask16x8_m128i_roundtrip, mask16x8, __m128i, i16, 8);
-native_vector_roundtrip_exhaustive!(mask32x4_m128i_roundtrip, mask32x4, __m128i, i32, 4);
-native_vector_roundtrip_exhaustive!(mask64x2_m128i_roundtrip, mask64x2, __m128i, i64, 2);
+#[simd_test]
+fn mask32x4_mmask8_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask32x4<S>, __mmask8, 4>(simd, bits);
+    }
+}
 
-native_vector_roundtrip_interesting!(
-    mask8x32_m256i_roundtrip,
-    mask8x32,
-    __m256i,
-    i8,
-    32,
-    INTERESTING_32
-);
-native_vector_roundtrip_exhaustive!(mask16x16_m256i_roundtrip, mask16x16, __m256i, i16, 16);
-native_vector_roundtrip_exhaustive!(mask32x8_m256i_roundtrip, mask32x8, __m256i, i32, 8);
-native_vector_roundtrip_exhaustive!(mask64x4_m256i_roundtrip, mask64x4, __m256i, i64, 4);
+#[simd_test]
+fn mask64x2_mmask8_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask64x2<S>, __mmask8, 2>(simd, bits);
+    }
+}
 
-native_mask_roundtrip_exhaustive!(mask8x16_mmask16_roundtrip, mask8x16, __mmask16, 16);
-native_mask_roundtrip_exhaustive!(mask16x8_mmask8_roundtrip, mask16x8, __mmask8, 8);
-native_mask_roundtrip_exhaustive!(mask32x4_mmask8_roundtrip, mask32x4, __mmask8, 4);
-native_mask_roundtrip_exhaustive!(mask64x2_mmask8_roundtrip, mask64x2, __mmask8, 2);
-native_mask_roundtrip_interesting!(
-    mask8x32_mmask32_roundtrip,
-    mask8x32,
-    __mmask32,
-    32,
-    INTERESTING_32
-);
-native_mask_roundtrip_exhaustive!(mask16x16_mmask16_roundtrip, mask16x16, __mmask16, 16);
-native_mask_roundtrip_exhaustive!(mask32x8_mmask8_roundtrip, mask32x8, __mmask8, 8);
-native_mask_roundtrip_exhaustive!(mask64x4_mmask8_roundtrip, mask64x4, __mmask8, 4);
-native_mask_roundtrip_interesting!(
-    mask8x64_mmask64_roundtrip,
-    mask8x64,
-    __mmask64,
-    64,
-    INTERESTING_64
-);
-native_mask_roundtrip_interesting!(
-    mask16x32_mmask32_roundtrip,
-    mask16x32,
-    __mmask32,
-    32,
-    INTERESTING_32
-);
-native_mask_roundtrip_exhaustive!(mask32x16_mmask16_roundtrip, mask32x16, __mmask16, 16);
-native_mask_roundtrip_exhaustive!(mask64x8_mmask8_roundtrip, mask64x8, __mmask8, 8);
+#[simd_test]
+fn mask8x32_mmask32_roundtrip<S: Simd>(simd: S) {
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0x0000_0000);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0x0000_0001);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0x8000_0000);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0x0000_ffff);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0xffff_0000);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0x5555_5555);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0xaaaa_aaaa);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0x8000_aa55);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0xffff_ffff);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0xffff_ffff_0000_0000);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0xffff_ffff_8000_aa55);
+    assert_native_mask_roundtrip::<S, mask8x32<S>, __mmask32, 32>(simd, 0xffff_ffff_ffff_ffff);
+}
+
+#[simd_test]
+fn mask16x16_mmask16_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask16x16<S>, __mmask16, 16>(simd, bits);
+    }
+}
+
+#[simd_test]
+fn mask32x8_mmask8_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask32x8<S>, __mmask8, 8>(simd, bits);
+    }
+}
+
+#[simd_test]
+fn mask64x4_mmask8_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask64x4<S>, __mmask8, 4>(simd, bits);
+    }
+}
+
+#[simd_test]
+fn mask8x64_mmask64_roundtrip<S: Simd>(simd: S) {
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0x0000_0000_0000_0000);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0x0000_0000_0000_0001);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0x8000_0000_0000_0000);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0x0000_0000_ffff_ffff);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0xffff_ffff_0000_0000);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0x5555_5555_5555_5555);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0xaaaa_aaaa_aaaa_aaaa);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0x8000_0001_5555_aaab);
+    assert_native_mask_roundtrip::<S, mask8x64<S>, __mmask64, 64>(simd, 0xffff_ffff_ffff_ffff);
+}
+
+#[simd_test]
+fn mask16x32_mmask32_roundtrip<S: Simd>(simd: S) {
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0x0000_0000);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0x0000_0001);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0x8000_0000);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0x0000_ffff);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0xffff_0000);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0x5555_5555);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0xaaaa_aaaa);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0x8000_aa55);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0xffff_ffff);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0xffff_ffff_0000_0000);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0xffff_ffff_8000_aa55);
+    assert_native_mask_roundtrip::<S, mask16x32<S>, __mmask32, 32>(simd, 0xffff_ffff_ffff_ffff);
+}
+
+#[simd_test]
+fn mask32x16_mmask16_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask32x16<S>, __mmask16, 16>(simd, bits);
+    }
+}
+
+#[simd_test]
+fn mask64x8_mmask8_roundtrip<S: Simd>(simd: S) {
+    for bits in 0..=0xffff_u64 {
+        assert_native_mask_roundtrip::<S, mask64x8<S>, __mmask8, 8>(simd, bits);
+    }
+}
