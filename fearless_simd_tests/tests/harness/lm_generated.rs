81441cfd85bc277129397eaa6c13137a77483f7c
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-05-24 18:53:01 +0100

    Fix build after file rename

--- fearless_simd_tests/tests/harness/lm_generated.rs
+++ fearless_simd_tests/tests/harness/lm_generated.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 mod extended_512;
-mod mask_methods;
+mod mask_roundtrip;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod mask_roundtrip_x86;
 mod mod_256;
