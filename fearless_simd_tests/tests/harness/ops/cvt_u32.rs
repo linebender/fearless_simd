41988bd098be566d3ab1413241ef37e96bafe49d
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-08-07 21:07:52 +0100

    Add tests that excercise normal values in all lanes

--- fearless_simd_tests/tests/harness/ops/cvt_u32.rs
+++ fearless_simd_tests/tests/harness/ops/cvt_u32.rs
@@ -7,6 +7,43 @@ use fearless_simd_dev_macros::simd_test;
 // One concrete test row per supported vector type.
 
 #[simd_test]
+fn cvt_u32_f32x4_regular<S: Simd>(simd: S) {
+    let a = f32x4::from_slice(simd, &[101.75, 22.5, 33.875, 404.25]);
+    assert_eq!(*a.to_int::<u32x4<_>>(), [101, 22, 33, 404]);
+}
+
+#[simd_test]
+fn cvt_u32_f32x8_regular<S: Simd>(simd: S) {
+    let a = f32x8::from_slice(
+        simd,
+        &[
+            801.75, 702.5, 603.25, 504.875, 405.125, 306.5, 207.75, 108.25,
+        ],
+    );
+    assert_eq!(
+        *a.to_int::<u32x8<_>>(),
+        [801, 702, 603, 504, 405, 306, 207, 108]
+    );
+}
+
+#[simd_test]
+fn cvt_u32_f32x16_regular<S: Simd>(simd: S) {
+    let a = f32x16::from_slice(
+        simd,
+        &[
+            1601.75, 1502.5, 1403.25, 1304.875, 1205.125, 1106.5, 1007.75, 908.25, 809.5, 710.75,
+            611.125, 512.875, 413.25, 314.5, 215.75, 116.25,
+        ],
+    );
+    assert_eq!(
+        *a.to_int::<u32x16<_>>(),
+        [
+            1601, 1502, 1403, 1304, 1205, 1106, 1007, 908, 809, 710, 611, 512, 413, 314, 215, 116,
+        ]
+    );
+}
+
+#[simd_test]
 fn cvt_u32_f32x4<S: Simd>(simd: S) {
     let a = f32x4::from_slice(simd, &[1.0, 42.7, 3e9, -0.3]);
     assert_eq!(*a.to_int::<u32x4<_>>(), [1, 42, 3000000000, 0]);
