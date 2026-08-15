e265ab515321b14c7d3ab7aa77b3c75022031291
David Anderson <dave@natulte.net>
2026-08-15 14:46:51 -0700

    Fix example link in readme
    Fixes #326
    
    Signed-off-by: David Anderson <dave@natulte.net>

--- fearless_simd/src/lib.rs
+++ fearless_simd/src/lib.rs
@@ -173,7 +173,7 @@
 //! AVX2 and AVX-512 provide more efficient instructions for some operations,
 //! and AVX-512 also more than doubles the number of vector registers of all sizes.
 //!
-//! You can also [disable certain instruction sets for select functions](https://github.com/linebender/fearless_simd/blob/main/fearless_simd/examples/sigmoid.rs)
+//! You can also [disable certain instruction sets for select functions](https://github.com/linebender/fearless_simd/blob/main/fearless_simd/examples/disable_avx2_for_one_function.rs)
 //! without disabling them globally.
 //!
 //! # Feature Flags
