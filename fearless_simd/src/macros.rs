178d0e7de260d587a9319969550ccc8460056b7a
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-09-13 07:12:37 +0000

    Minor fixes (#377)
    I had Astra review the codebase for bugs and this is what it flagged.
    
    1. A syntheric feature flag configuration can produce a panic in
    dispatcher due to a desync between feature strings
    2. Sigmoid example didn't demonstrate the scalar remainder handling,
    which I feel we should include in the example code

--- fearless_simd/src/macros.rs
+++ fearless_simd/src/macros.rs
@@ -194,6 +194,7 @@ macro_rules! __fearless_simd_dispatch_dispatch_avx512 {
     any(
         disable_dispatch_avx512,
         not(all(
+            target_feature = "adx",
             target_feature = "aes",
             target_feature = "avx512bitalg",
             target_feature = "avx512bw",
@@ -241,6 +242,7 @@ macro_rules! __fearless_simd_dispatch_dispatch_avx2 {
     disable_dispatch_avx2,
     all(
         not(disable_dispatch_avx512),
+        target_feature = "adx",
         target_feature = "aes",
         target_feature = "avx512bitalg",
         target_feature = "avx512bw",
